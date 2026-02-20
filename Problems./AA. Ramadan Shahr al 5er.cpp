8fbda7192f652672fc3ac7382f6ba5b6059c50b7
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-21 00:14:44 +0200

    Create AA. Ramadan Shahr al 5er.cpp

---
+++ Problems./AA. Ramadan Shahr al 5er.cpp
@@ -0,0 +1,24 @@
+    // AA. Ramadan Shahr al 5er
+    // URL: https://codeforces.com/group/93ThDH8Tvv/contest/672841/problem/AA
+    // Time: 2/21/2026, 00:41:31 
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define int         long long
+    #define vi          vector<int>
+    #define pii         pair<int, int>
+    #define all(v)      v.begin(), v.end()
+    #define finish(s)   { cout << s << "\n"; return; }
+    template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+    template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+    void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+    void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+    const int MOD = 1000000007, INF = 2e18;
+    void solve() {
+       int a , b; cin >> a >> b;
+       cout <<b/a<<"\n";
+    }
+    //
+    signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+        solve();
+    }
