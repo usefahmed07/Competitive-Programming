53d68731ce3fcd47518da9bcd74936336005c32e
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-21 14:55:59 +0200

    Create A. Fishing Trip.cpp

---
+++ Problems./A. Fishing Trip.cpp
@@ -0,0 +1,33 @@
+
+        // A. Fishing Trip
+        // URL: https://codeforces.com/group/mOayoTuNLs/contest/455027/problem/A
+        // Time: 2/21/2026, 14:36:31 
+        #include <bits/stdc++.h>
+        using namespace std;
+        #define int         long long
+        #define int long long
+        #define vi          vector<int>
+        #define pii         pair<int, int>
+        #define all(v)      v.begin(), v.end()
+        #define finish(s)   { cout << s << "\n"; return; }
+        template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+        template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+        void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+        void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+        const int MOD = 1000000007, INF = 2e18;
+        void solve() {
+        int tt; cin >> tt;
+        while (tt--){
+            int n; cin >> n;
+            if (n == 0){
+                cout <<"Yes"<<"\n";
+            }
+            else {
+                cout <<"No"<<"\n";
+            }
+        }
+    }
+        signed main() {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+            solve();
+        }
