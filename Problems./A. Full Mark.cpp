04814b01221a38916f1bbf9af254f5605cadcc08
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-21 14:51:26 +0200

    Create A. Full Mark.cpp

---
+++ Problems./A. Full Mark.cpp
@@ -0,0 +1,32 @@
+    // A. Full Mark
+    // URL: https://codeforces.com/group/mOayoTuNLs/contest/452285/problem/A
+    // Time: 2/21/2026, 14:36:31 
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define int         long long
+    #define int long long
+    #define vi          vector<int>
+    #define pii         pair<int, int>
+    #define all(v)      v.begin(), v.end()
+    #define finish(s)   { cout << s << "\n"; return; }
+    template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+    template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+    void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+    void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+    const int MOD = 1000000007, INF = 2e18;
+    void solve() {
+    int tt; cin >> tt;
+    while (tt--){
+        int n; cin >> n;
+        if (n > 0){
+             cout <<"10"<<"\n";
+        }
+        else if (n == 0){
+            cout <<"0"<<"\n";
+        }
+    }
+}
+    signed main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        solve();
+    }
