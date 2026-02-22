9462f0ad681286117c88ba76c8dd5707749cd9d1
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-22 02:44:57 +0200

    Create A. Zaglol welcoming.cpp

---
+++ Problems./A. Zaglol welcoming.cpp
@@ -0,0 +1,24 @@
+    // A. Zaglol welcoming
+    // URL: https://codeforces.com/gym/106351/problem/A
+    // Time: 2/22/2026, 02:36:31 
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define int         long long
+    #define int long long
+    #define vi          vector<int>
+    #define pii         pair<int, int>
+    #define all(v)      v.begin(), v.end()
+    #define finish(s)   { cout << s << "\n"; return; }
+    template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+    template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+    void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+    void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+    const int MOD = 1000000007, INF = 2e18;
+    void solve() {
+    string x; cin >> x;
+    cout <<"FCDS"<<"\n";
+    }
+        signed main() {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+            solve();
+        }
