c9a937524b97ceed55a83fe52b2e8d9a0d11d241
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-24 01:43:21 +0200

    Create A. ecpc sofra.cpp

---
+++ Problems./A. ecpc sofra.cpp
@@ -0,0 +1,24 @@
+        // A. ecpc sofra
+        // URL: https://codeforces.com/group/mOayoTuNLs/contest/669072/problem/A
+        // Time: 2/24/2026, 02:36:31 
+        #include <bits/stdc++.h>
+        using namespace std;
+        #define int         long long
+        #define int long long
+        #define vi          vector<int>
+        #define pii         pair<int, int>
+        #define all(v)      v.begin(), v.end()
+        #define finish(s)   { cout << s << "\n"; return; }
+        template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+        template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+        void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+        void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+        const int MOD = 1000000007, INF = 2e18;
+        void solve() {
+        int n; cin >> n;
+        cout <<n*3<<"\n";
+        }
+            signed main() {
+            ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+                solve();
+            }
