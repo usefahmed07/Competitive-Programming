d372d5987a67f2f7a35a5474b7f7cd29e84bdc7a
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-24 03:22:43 +0200

    Create A. Submission is All You Need II.cpp

---
+++ Problems./A. Submission is All You Need II.cpp
@@ -0,0 +1,27 @@
+// A. Submission is All You Need II
+// URL: https://codeforces.com/gym/105846/problem/A
+// Time: 2/24/2026, 02:56:42 PM
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+// fixed << setprecision()
+const string IN = "input.txt", OUT = "output.txt";
+#define tc(test) ll t = 1; if (test) cin >> t; while (t--) ASAAD()
+#define FIn cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+#define file freopen("promote.in", "r", stdin); freopen("promote.out", "w", stdout)
+#define OJudge(in,out) freopen(in, "r", stdin); freopen(out, "w", stdout)
+void Usif() {
+    int t; cin >> t;
+    while (t--){
+        ll n; cin >> n;
+        cout <<n<<"\n";
+    }
+}
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    Usif();
+    return 0;
+}
