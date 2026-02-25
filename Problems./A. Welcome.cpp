80738de4c95a4b919f73ca188c16f024eff4fa6c
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-02-26 01:10:30 +0200

    Create A. Welcome.cpp

---
+++ Problems./A. Welcome.cpp
@@ -0,0 +1,24 @@
+// A. Welcome
+// URL: https://codeforces.com/group/MEqF8b6wBT/contest/673206/problem/A4
+// Time: 2/26/2026, 00:55:42 PM
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define el '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+// fixed << setprecision()
+const string IN = "input.txt", OUT = "output.txt";
+#define tc(test) ll t = 1; if (test) cin >> t; while (t--) ASAAD()
+#define FIn cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+#define file freopen("promote.in", "r", stdin); freopen("promote.out", "w", stdout)
+#define OJudge(in,out) freopen(in, "r", stdin); freopen(out, "w", stdout)
+void Usif() {
+  string s; 
+  getline(cin, s); cout <<s <<"\n";
+}
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    Usif();
+    return 0;
+}
