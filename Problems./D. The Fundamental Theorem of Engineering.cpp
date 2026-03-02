2c4e9aa146e23099840b0f72feeb4c611afecd4f
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-02 03:01:40 +0200

    Create D. The Fundamental Theorem of Engineering.cpp

---
+++ Problems./D. The Fundamental Theorem of Engineering.cpp
@@ -0,0 +1,32 @@
+    // D.The Fundamental Theorem of Engineering
+    // URL: https://codeforces.com/group/Rilx5irOux/contest/541733/problem/A
+    // Time: 3/2/2026, 02:56:42 AM
+    #include <bits/stdc++.h>
+    using namespace std;
+    #define ll long long
+    #define el '\n'
+    #define all(x) (x).begin(), (x).end()
+    #define rall(x) (x).rbegin(), (x).rend()
+    // fixed << setprecision()
+    const string IN = "input.txt", OUT = "output.txt";
+    #define tc(test) ll t = 1; if (test) cin >> t; while (t--) ASAAD()
+    #define FIn cin.tie(0);cout.tie(0);ios_base::sync_with_stdio(false);
+    #define file freopen("promote.in", "r", stdin); freopen("promote.out", "w", stdout)
+    #define OJudge(in,out) freopen(in, "r", stdin); freopen(out, "w", stdout)
+    void Usif() {
+    string x; cin >> x;
+    if (x == "e"){
+      cout <<"3"<<"\n";
+    }
+    else if (x == "g"){
+      cout <<"9"<<"\n";
+    }
+    else {
+      cout <<"3"<<"\n";
+    }
+    }
+    int main() {
+        ios::sync_with_stdio(false); cin.tie(nullptr);
+        Usif();
+        return 0;
+    }
