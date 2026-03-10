c40cdba29afaca9a3477a68a2db7e547121dbe7b
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-10 04:04:49 +0200

    Create F. Equal Distribution.cpp

---
+++ Problems./F. Equal Distribution.cpp
@@ -0,0 +1,29 @@
+// H. Welcome to school
+// URL: https://codeforces.com/group/Rilx5irOux/contest/636158/problem/H
+// Time: 3/10/2026, 03:51:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int         long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int k,n; cin >> k >> n;   
+    cout << (k / n) * n << endl;
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
