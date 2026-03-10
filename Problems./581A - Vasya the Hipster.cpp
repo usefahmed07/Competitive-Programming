71e78a6badc7217e31d8794e029f4ec34ee10ae3
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-10 03:48:06 +0200

    Create  581A - Vasya the Hipster.cpp

---
+++ Problems./581A - Vasya the Hipster.cpp
@@ -0,0 +1,32 @@
+// A. Vasya the Hipster
+// URL: https://codeforces.com/problemset/problem/581/A
+// Time: 3/10/2026, 00:51:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int         long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+        int a ,b; cin >> a >> b;
+        int different = min(a, b);
+        int same = abs(a - b) / 2;
+
+    cout << different << " " << same <<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
