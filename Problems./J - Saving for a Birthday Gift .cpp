fc05274f1f886df3a57ce4044b1112a7055b1570
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-14 23:38:01 +0200

    Create  J - Saving for a Birthday Gift .cpp

---
+++ Problems./J - Saving for a Birthday Gift .cpp
@@ -0,0 +1,35 @@
+// J. Saving for a Birthday Gift
+// URL: https://codeforces.com/group/Rilx5irOux/contest/638270/problem/J
+// Time: 3/14/2026, 16:51:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+   int a , b , c; cin >> a >> b >> c;
+   if ((a*b)>=c){
+    cout <<"YES"<<"\n";
+   }
+   else {
+    cout <<"NO"<<"\n";
+   }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
