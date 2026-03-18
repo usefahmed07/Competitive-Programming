242276b54747f96963c939f685d65d7d83e19afd
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-18 05:51:55 +0200

    Create B - Ramadan Lights.cpp

---
+++ Problems./B - Ramadan Lights.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int count = 0;
+    for (char c : s) {
+        if (c == '1') count++;
+    }
+
+    cout << count <<"\n";
+}
