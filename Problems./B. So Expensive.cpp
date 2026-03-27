3a28aba3806c34ab15940b7c452199da02dfb797
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-27 21:11:52 +0200

    Create B. So Expensive.cpp

---
+++ Problems./B. So Expensive.cpp
@@ -0,0 +1,35 @@
+// B. So Expensive
+// URL: https://codeforces.com/group/oLgsztGOo0/contest/210468/problem/B
+// Time: 3/27/2026, 17:20:42 AM
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int B , T , J; cin >> B >> T >> J;
+    if (B >= (T+J)){
+        cout <<"Lucky Zain"<<"\n";
+    }
+    else {
+        cout <<"Poor Zain"<<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
