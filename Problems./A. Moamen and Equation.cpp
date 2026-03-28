1bafea84daf35090d34ef87cf5b1025fd3737d07
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-03-29 00:18:42 +0200

    Create A. Moamen and Equation.cpp

---
+++ Problems./A. Moamen and Equation.cpp
@@ -0,0 +1,35 @@
+// A. Moamen and Equation
+// URL: https://codeforces.com/group/UZxBcPr4qR/contest/248138/problem/A
+// Date: 3/29/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    long long a, b, c, d, n;
+    cin >> a >> b >> c >> d >> n;
+
+    long long ans = ((a % n) * (b % n)) % n;
+    ans = (ans + ((c % n) * (d % n)) % n) % n;
+
+    cout << ans << "\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
