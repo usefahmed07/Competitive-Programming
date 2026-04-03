75c9719a79014bb419d957906fdca2f3588b2804
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-03 23:01:08 +0200

    Create D. First Accepted?.cpp

---
+++ Problems./D. First Accepted?.cpp
@@ -0,0 +1,33 @@
+// D. First Accepted?
+// URL: https://codeforces.com/group/n8KAvVSzzc/contest/397575/problem/D
+// Date: 4/3/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int t; cin >> t;
+    while (t--){
+    ll a , b , c , d , e , f , j; cin >> a >> b >> c >> d >> e >> f >> j;
+    cout << a + b + c + d + e + f + j <<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
