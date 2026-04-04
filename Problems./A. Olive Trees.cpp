ada9dd43824552a9e7c67105d660d29696e9829e
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-04 08:07:55 +0200

    Create A. Olive Trees.cpp

---
+++ Problems./A. Olive Trees.cpp
@@ -0,0 +1,30 @@
+// A. Olive Trees
+// URL: https://codeforces.com/group/xVLE81LHxg/contest/240104/problem/A
+// Date: 4/4/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    int a , b , c; cin >> a >> b >> c;
+    cout <<max ({a,b,c})<<"\n";
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
