b1102f011287d03871a999093b00d18ecb652c86
Youssef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-06 02:24:09 +0200

    Create K - Yasser And Fin Math .cpp

---
+++ Problems./K - Yasser And Fin Math .cpp
@@ -0,0 +1,36 @@
+// K. Yasser And Fin Math
+// URL: https://codeforces.com/group/3jD9SzY31n/contest/249678/problem/K
+// Date: 4/6/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    ll x , y; cin >> x >> y;
+    ll K = (x+y)/2;
+    if ((x+y)%2==0){
+        cout << K <<"\n";
+    }
+    else {
+        cout <<"IMPOSSIBLE"<<"\n";
+    }
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
