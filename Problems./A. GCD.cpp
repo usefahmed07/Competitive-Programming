e7f9ccbbb0b26adaa0b0b81d7fc4f639acec4cc6
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-04-08 20:58:19 +0200

    Create A. GCD.cpp

---
+++ Problems./A. GCD.cpp
@@ -0,0 +1,32 @@
+// A. GCD
+// URL: https://codeforces.com/group/gALKjLf4oD/contest/237861/problem/A
+// Date: 4/8/2026
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve() {
+    ll a , b , x; cin >> a >> b; 
+    ll g = __gcd(a, b);
+    ll l = (a / g) * b;
+    cout << g << " " << l;
+}
+/*
+ 
+*/
+signed main() {
+    // UseFile();
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    solve();
+    return 0;
+}
