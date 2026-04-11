54c50aa658c0c5ad5f46c1390c9c14f41f672750
usefahmed07 <youssef.ahmed.hamaza@gmail.com>
2026-04-11 14:38:56 +0200

    Create G. Shams Al-Ma'arif.cpp

---
+++ Problems./G. Shams Al-Ma'arif.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long r;
+    cin >> r;
+ 
+    double pi = 3.141592;
+    double area = 4 * pi * r * r;
+ 
+    cout << (long long)(area + 0.5) << "\n";
+    return 0;
+}
