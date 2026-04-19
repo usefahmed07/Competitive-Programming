5d7e6d26e0e02d4f81a731d3497deb9e7f9740e0
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-19 21:21:07 +0200

    Delete LeetCodeSQL50 directory

--- LeetCodeSQL50/LeetCode 1757: Recyclable and Low Fat Products [SQL] .sql
+++
@@ -1,7 +0,0 @@
-// LeetCode 1757: Recyclable and Low Fat Products [SQL] 
-// URL : https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
-// Date : 4/4/2026
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y'
-AND recyclable = 'Y';
