7cfb621b56814de598bf2fa350df648cdb1a5e88
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-04-28 23:53:53 +0300

    Create A. Hello, Harbin Institute of Technology!.cpp

---
+++ Problems./A. Hello, Harbin Institute of Technology!.cpp
@@ -0,0 +1,32 @@
+/**
+      A. Hello, Harbin Institute of Technology!
+ *    author:  BetterCallUssif
+ *    created: 28.04.2026 17:18:32
+      URL : https://codeforces.com/gym/106290/problem/A
+**/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ll  long long
+#define vi          vector<int>
+#define pii         pair<int, int>
+#define all(v)      v.begin(), v.end()
+#define finish(s)   { cout << s << "\n"; return; }
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v) { for (auto& i : v) os << i << ' '; return os; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& i : v) is >> i; return is; }
+void FastIO() { cin.tie(nullptr)->sync_with_stdio(false); }
+void UseFile() { freopen("file.in", "r", stdin); freopen("file.out", "w", stdout); }
+const int MOD = 1000000007, INF = 2e18;
+// fixed << setprecision()
+void solve (){
+    cout <<"hello hlt"<<"\n";
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
