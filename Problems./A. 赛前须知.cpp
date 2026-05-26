dc9ea3bf9dc75ac161ec2833ddd7d511bf3deae5
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-05-26 14:23:20 +0300

    Create A. 赛前须知.cpp

---
+++ Problems./A. 赛前须知.cpp
@@ -0,0 +1,47 @@
+/** 
+    A. 赛前须知
+ *  author:  BetterCallUssif
+ *  created: 26.05.2026
+    URL : https://codeforces.com/gym/104823/problem/A
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve (){
+    cout << "I have read the rules of the competition above and fully understand the meaning of each rule. I understand and acknowledge the following:" <<"\n";
+    cout << "1. Each player on my team can use one computer for the competition, and multiple computers can be used simultaneously." <<"\n";
+    cout << "2. During the competition, I can use the Internet but cannot communicate with other teams." <<"\n";
+    cout << "3. The problems in this competition are not ordered by difficulty, so if I encounter a problem I don't know how to solve, I should continue reading the next problem." <<"\n";
+
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    return 0;
+}
