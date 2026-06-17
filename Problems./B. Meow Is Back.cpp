a0dbbcadb6014e789ea9c6b0a31acfde8a04c5a2
Yousssf Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-06-17 17:27:55 +0300

    Add B. Meow Is Back problem solution

---
+++ Problems./B. Meow Is Back.cpp
@@ -0,0 +1,50 @@
+/** 
+    B. Meow Is Back
+ *  author:  BetterCallUssif
+ *  created: 17.06.2026
+    URL : https://codeforces.com/group/3augpdtPJt/contest/428591/problem/B
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    string x; cin >> x;
+    if (x == "meow"){
+        cout <<"meow!"<<"\n";
+    } 
+    else {
+        cout <<"mew?"<<"\n";
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
