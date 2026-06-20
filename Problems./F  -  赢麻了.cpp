68468135afdd13b0a1d67740b9e0ba00fb7b47bb
Yousef Ahmed <youssef.ahmed.hamaza@gmail.com>
2026-06-20 20:08:59 +0300

    Create F - 赢麻了.cpp

---
+++ Problems./F  -  赢麻了.cpp
@@ -0,0 +1,57 @@
+/** 
+    F. Matrix Bel Lotus
+ *  author:  BetterCallUssif
+ *  created: 20.06.2026
+    URL : https://codeforces.com/gym/106094/problem/A
+**/
+/// I am snatchin chains and burnin' tattoos
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define F first
+#define S second
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define sz(x) (int)(x).size()
+#define mp(x, y) make_pair(x, y)
+#define popCnt(x) (__builtin_popcountll(x))
+#define int ll
+ 
+using ll = long long;
+using ii = pair<int, int>;
+using ull = unsigned long long;
+ 
+const int N = 2e6 + 5, A = 26, LG = 19, MOD = 1e9 + 7;
+const long double PI = acos(-1);
+const long double EPS = 1e-7;
+// fixed << setprecision()
+void solve() {
+    int tt;
+    cin >> tt;
+
+    while (tt--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if (a > b)
+            cout << "Win\n";
+        else if (c > b)
+            cout << "WIN\n";
+        else
+            cout << "nowin\n";
+    }
+}
+/*
+*/
+signed main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("guess.out", "w", stdout);
+    solve();
+    // just normal comment for nonsense readers...
+    return 0;
+}
